9b7e72725e451712775060dd9e00255816aad4f9
Roman Tuomisto <romantuomisto@gmail.com>
2022-09-22 06:45:07 +0300

    adds dburl

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
-const MongoDBStore = require("connect-mongodb-session") (session);
+const MongoDBStore = require("connect-mongodb-session");
 
 const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp1";
 
